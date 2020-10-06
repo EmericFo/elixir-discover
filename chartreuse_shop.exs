df1b6b1a65bcd8ccb5ccc944f8f06c67c81bf69f
Emeric Fontaine <emeric.fontaine@orange.com>
2020-10-06 15:43:02 +0200

    map exemple on alcool degres

--- chartreuse_shop.exs
+++ chartreuse_shop.exs
@@ -10,11 +10,24 @@ defmodule Chartreuse_shop do
             send from_pid, 42
 
       {:alcool, product, from_pid}->
-        send from_pid, {"verte", "jaune"} # with map
+        reference = %{
+          "Verte" => 55,
+          "ODF" => 45,
+          "Liqueur" => 56
+        }
+
+        result = Map.get(reference, product, "Unknow references")
+
+        send from_pid, {:has_degrees, result} # with map
         # extract fonction
 
 
     end
     next_message()
   end
+
+  def open do
+    spawn Chartreuse_shop, :next_message, []
+  end
+
 end
--- chartreuse_shop_customer.exs
+++ chartreuse_shop_customer.exs
@@ -5,11 +5,17 @@ defmodule Chartreuse_shop_customer do
 
       :order_ok ->
         IO.puts "[#{inspect self}] command ok"
+      {:has_degrees, degrees} ->
+        IO.puts "[#{inspect self}] #{degrees}"
       42 ->
-        IO.puts "Universal answer"
+        IO.puts "[#{inspect self}] Universal answer"
         _ ->
-          IO.puts "don't know what to do"
+          IO.puts "[#{inspect self}] don't know what to do"
     end
     shop_answer()
+  end
+
+  def new_customer do
+    spawn Chartreuse_shop_customer, :shop_answer, []
   end
 end
